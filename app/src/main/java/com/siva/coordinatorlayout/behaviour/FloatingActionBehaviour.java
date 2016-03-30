411a4c1e958d94fb1a216a07e2b23b147b5e7cca
kseesala <seesala.kiran@gmail.com>
2016-03-30 00:24:34 -0500

    Initial commit

---
+++ app/src/main/java/com/siva/coordinatorlayout/behaviour/FloatingActionBehaviour.java
@@ -0,0 +1,30 @@
+package com.siva.coordinatorlayout.behaviour;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by ksees_000 on 3/29/2016.
+ */
+public class FloatingActionBehaviour extends CoordinatorLayout.Behavior<FloatingActionButton> {
+
+    public FloatingActionBehaviour(Context context, AttributeSet attributeSet) {
+
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
+        return dependency instanceof Snackbar.SnackbarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
+        float translationY = Math.min(0, dependency.getTranslationY() - dependency.getHeight());
+        child.setTranslationY(translationY);
+        return true;
+    }
+}
